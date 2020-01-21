5faa52c106349b942f17b3d3fe4b51580eebaa3c
alcfernandes <alessandro.c.fernandes@gmail.com>
2020-01-21 10:10:09 -0300

    feat(api): Cria recurso meios-de-recebimento na API (rea-donly)
    Cria o serializer, viewset e endpoint para o recurso
    meios-de-recebimento.

--- sme_uniforme_apps/core/api/urls.py
+++ sme_uniforme_apps/core/api/urls.py
@@ -2,6 +2,7 @@ from rest_framework import routers
 from django.urls import include, path
 from .viewsets.version_viewset import ApiVersion
 from .viewsets.uniformes_viewset import UniformesViewSet
+from .viewsets.meios_de_recebimento_viewset import MeiosDeRecebimentoViewSet
 
 # Importe aqui as rotas das demais aplicações
 from sme_uniforme_apps.proponentes.urls import router as proponentes_router
@@ -10,6 +11,7 @@ router = routers.DefaultRouter()
 
 router.register('api-version', ApiVersion, basename='Version')
 router.register('uniformes', UniformesViewSet)
+router.register('meios-de-recebimento', MeiosDeRecebimentoViewSet)
 
 # Adicione aqui as rotas das demais aplicações para que as urls sejam exibidas na API Root do DRF
 router.registry.extend(proponentes_router.registry)
--- sme_uniforme_apps/core/tests/conftest.py
+++ sme_uniforme_apps/core/tests/conftest.py
@@ -6,3 +6,8 @@ from model_bakery import baker
 @pytest.fixture
 def uniforme():
     return baker.make('Uniforme', nome='Calça')
+
+
+@pytest.fixture
+def meio_de_recebimento():
+    return baker.make('MeioDeRecebimento', nome='Visa')
