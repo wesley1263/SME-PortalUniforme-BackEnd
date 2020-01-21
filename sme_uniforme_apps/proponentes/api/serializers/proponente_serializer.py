8d4fbabb78279d6810085775fb785bc6de934286
alcfernandes <alessandro.c.fernandes@gmail.com>
2020-01-21 14:07:33 -0300

    feat(api):Criação meios de recebimento por POST de proponentes
    Altera o serializer de proponentes para gravar os meios de
    recebimento em um POST.

--- sme_uniforme_apps/proponentes/api/serializers/proponente_serializer.py
+++ sme_uniforme_apps/proponentes/api/serializers/proponente_serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
-from ...models import Proponente
+from ...models import Proponente, Anexo
 from ...api.serializers.oferta_de_uniforme_serializer import OfertaDeUniformeSerializer
 from ...api.serializers.loja_serializer import LojaSerializer
 from ...api.serializers.anexo_serializer import AnexoSerializer
@@ -19,6 +20,30 @@ class ProponenteSerializer(serializers.ModelSerializer):
 
 
 class ProponenteCreateSerializer(serializers.ModelSerializer):
+
+    # arquivos_anexos = serializers.ListField(
+    #     child=AnexoSerializer()
+    # )
+
+    def create(self, validated_data):
+        arquivos_anexos = validated_data.pop('arquivos_anexos', [])
+        meios_de_recebimento_list = validated_data.pop('meios_de_recebimento', [])
+
+        proponente = Proponente.objects.create(**validated_data)
+
+        tamanho_total_dos_arquivos = 0
+        print('inicio', arquivos_anexos)
+        for anexo in arquivos_anexos:
+            file_size = anexo.get('arquivo').size
+            tamanho_total_dos_arquivos += file_size
+            if tamanho_total_dos_arquivos > 10485760:
+                raise ValidationError("O tamanho total máximo dos arquivos é 10MB")
+            print('Arquivo', anexo.get('arquivo').name, anexo.get('arquivo').size)
+            Anexo.objects.create(proponente=proponente, arquivo=anexo.get("arquivo"))
+
+        proponente.meios_de_recebimento.set(meios_de_recebimento_list)
+
+        return proponente
 
     class Meta:
         model = Proponente
--- sme_uniforme_apps/proponentes/api/viewsets/proponentes_viewset.py
+++ sme_uniforme_apps/proponentes/api/viewsets/proponentes_viewset.py
@@ -2,6 +2,7 @@ from django_filters import rest_framework as filters
 
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
 
@@ -12,6 +13,7 @@ from ...models import Proponente
 
 
 class ProponentesViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     lookup_field = 'uuid'
     proponentes_queryset = Proponente.objects.all()
 
