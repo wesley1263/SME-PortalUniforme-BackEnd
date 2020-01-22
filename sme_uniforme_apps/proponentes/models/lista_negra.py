34bb4da5f1a8d489e9011474500fe4fd54850e90
Alessandro Fernandes <alcfernandes@yahoo.com>
2020-01-22 20:52:16 -0300

    feat(proponentes): Cria cadastro de cnpj's bloqueados
    Cria o modelo e disponibiliza no Admin do sistema uma tabela de cnpj's
    bloqueados para
    o credenciamento (lista negra)

---
+++ sme_uniforme_apps/proponentes/models/lista_negra.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from .validators import cnpj_validation
+
+from sme_uniforme_apps.core.models_abstracts import ModeloBase
+
+
+class ListaNegra(ModeloBase):
+
+    cnpj = models.CharField(
+        "CNPJ", max_length=20, validators=[cnpj_validation], blank=True, null=True, default="", unique=True
+    )
+    razao_social = models.CharField("Razão Social", max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.cnpj} - {self.razao_social}"
+
+    class Meta:
+        verbose_name = "CNPJ bloqueado"
+        verbose_name_plural = "CNPJ's bloqueados"
