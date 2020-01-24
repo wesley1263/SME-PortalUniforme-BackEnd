c525508f489d44a4687d96dd0904d5c539be5db8
wesley1263 <weslei.souza@amcom.com.br>
2020-01-24 08:10:23 -0300

    Adiciona permissão para endpoints restritos

--- sme_uniforme_apps/core/api/viewsets/meios_de_recebimento_viewset.py
+++ sme_uniforme_apps/core/api/viewsets/meios_de_recebimento_viewset.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
-
 
 from ..serializers.meio_de_recebimento_serializer import MeioDeRecebimentoSerializer, MeioDeRecebimentoLookUpSerializer
 from ...models import MeioDeRecebimento
@@ -10,11 +10,9 @@ from ...models import MeioDeRecebimento
 
 class MeiosDeRecebimentoViewSet(viewsets.ReadOnlyModelViewSet):
     lookup_field = 'id'
-
     queryset = MeioDeRecebimento.objects.all()
-
     serializer_class = MeioDeRecebimentoSerializer
-
+    permission_classes = [AllowAny]
     filter_backends = (SearchFilter, OrderingFilter)
     ordering_fields = ('nome',)
     search_fields = ('uuid', 'id', 'nome')
